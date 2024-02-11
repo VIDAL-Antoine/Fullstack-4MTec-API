7efbd29ca72f383bb5772800ddea9c7982038487
VIDAL-Antoine <avidal78390@gmail.com>
2024-02-12 00:10:08 +0100

    Meilleur visuel pour les formulaires signup et login

--- client/src/components/SignUpPage.vue
+++ client/src/components/SignUpPage.vue
@@ -1,12 +1,27 @@
 <template>
-    <v-container>
-      <v-text-field v-model="username" label="Username" required hide-details></v-text-field>
-      <v-text-field v-model="password" label="Password" hide-details required type="password"></v-text-field>
-      <v-btn type="submit" color="primary" block class="mt-2" @click="signup">Sign Up</v-btn>
-      <br>
-      <div v-if="errorMessage" class="error-message">/!\ {{ errorMessage }} /!\</div>
-    </v-container>
-</template>
+   <v-container>
+     <v-row>
+       <v-col cols="12" sm="6" offset-sm="3">
+         <v-text-field v-model="username" label="Username" required hide-details></v-text-field>
+       </v-col>
+     </v-row>
+     <v-row>
+       <v-col cols="12" sm="6" offset-sm="3">
+         <v-text-field v-model="password" label="Password" hide-details required type="password"></v-text-field>
+       </v-col>
+     </v-row>
+     <v-row>
+       <v-col cols="12" sm="6" offset-sm="3">
+         <v-btn type="submit" color="primary" block class="mt-2" @click="signup">Sign Up</v-btn>
+       </v-col>
+     </v-row>
+     <v-row>
+       <v-col cols="12" sm="6" offset-sm="3">
+         <v-alert v-if="errorMessage" type="error" class="mt-2">{{ errorMessage }}</v-alert>
+       </v-col>
+     </v-row>
+   </v-container>
+ </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
